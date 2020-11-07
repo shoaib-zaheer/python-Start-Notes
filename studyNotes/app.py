f87b66984d0574711b1bb50bf2cf9b9b8be30296
shoaib <z.shoaib015@gmail.com>
2020-11-07 12:02:19 +0100

    udpate

--- studyNotes/app.py
+++ studyNotes/app.py
@@ -1,4 +1,5 @@
-lucky_num = [4, 6, 8, 10, 12, 14, 16]
-time = ["Day", "Afternoon", "Midday", "Eveining", "Night"]
-time2 =time.copy()
-print(time2, time)
+tuple_one = (4, 5)
+tuple_two = [(3, 3),(9, 7),(4, 7)]
+tuple_two.insert(1,(5, 5))
+print(tuple_one)
+print(tuple_two)
