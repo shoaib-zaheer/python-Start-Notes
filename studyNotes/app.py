198782113d068fbc7e51736bc4da32fb863aeb4f
shoaib <z.shoaib015@gmail.com>
2021-01-13 01:52:00 +0100

    update day 6

--- studyNotes/app.py
+++ studyNotes/app.py
@@ -1,5 +1,5 @@
-tuple_one = (4, 5)
-tuple_two = [(3, 3),(9, 7),(4, 7)]
-tuple_two.insert(1,(5, 5))
-print(tuple_one)
-print(tuple_two)
+def say_hi():
+    print("Hello World")
+print ("top")
+say_hi()
+print("bottom")
